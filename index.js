56a305bf9a46fca56257b0edc22ea3d773b043c0
saichandrapandraju <saichandrapandraju@gmail.com>
2024-11-22 21:48:41 -0500

    a5

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 const app = express();
+console.log(process.env.NETLIFY_URL)
 app.use(
     cors({
       credentials: true,
